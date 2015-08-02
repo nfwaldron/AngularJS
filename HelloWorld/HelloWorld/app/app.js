c1dff21f492005f9f781dac0fd532d7e3b027eed
nfwaldron <waldron.nathan@gmail.com>
2015-08-02 01:27:06 -0700

    Store Application

---
+++ HelloWorld/HelloWorld/app/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var app = angular.module('gemStore', [])
+
+    //create controller
+    app.controller('StoreController', function () {
+        this.product = gem;
+
+    });
+
+    // create objects that will be represented in the real world
+    var gem = {
+        name:'Ruby',
+        price:3.50,
+        description:'....'
+    };
+
+})();
