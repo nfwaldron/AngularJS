f4b6c2e9f46ddb51663f1f7b422954dff009b35e
nfwaldron <waldron.nathan@gmail.com>
2015-08-02 16:38:49 -0700

    update

--- HelloWorld/HelloWorld/app/app.js
+++ HelloWorld/HelloWorld/app/app.js
@@ -3,15 +3,34 @@
 
     //create controller
     app.controller('StoreController', function () {
-        this.product = gem;
+        this.products = gems;
 
     });
 
     // create objects that will be represented in the real world
-    var gem = {
-        name:'Ruby',
-        price:3.50,
-        description:'....'
-    };
+    var gems = [
+         {
+             name: 'Ruby',
+             price: 3.50,
+             description: '....',
+             canPurchase: false,
+             soldOut: true
+         },
+
+        {
+            name: 'Diamond',
+            price: 4.50,
+            description: '....',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Emerald',
+            price: 7.25,
+            description: '....',
+            canPurchase: true,
+            soldOut: false
+        }
+    ];
 
 })();
