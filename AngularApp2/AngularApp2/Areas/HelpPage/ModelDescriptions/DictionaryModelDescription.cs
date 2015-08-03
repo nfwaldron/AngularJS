c09b577e5b177ccc2d45a4a47249b27bbcb7a8bb
nfwaldron <waldron.nathan@gmail.com>
2015-08-03 13:15:14 -0700

    update

---
+++ AngularApp2/AngularApp2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularApp2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AngularApp2/AngularApp2/ngApps/CountriesApp/controllers.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    angular.module('CountriesApp').controller('CountriesController', function () {
+        this.countries = countries;
+
+        });
+    var countries = [
+        {name:'Trinidad',
+        population: 120000},
+        {name: 'USA',
+        population: 6000000},
+        {name: 'Indonesia',
+        population: 130000
+        }
+    ]
+
+    this.chooseColor = function (selectedColor) {
+        alert('You chose' + selectedColor);
+    }
+
+})();
