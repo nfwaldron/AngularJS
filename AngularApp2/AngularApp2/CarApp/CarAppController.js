60319329274f13dd2b8c6a8a501a1137c76a17dd
nfwaldron <waldron.nathan@gmail.com>
2015-08-03 14:56:46 -0700

    update

---
+++ AngularApp2/AngularApp2/CarApp/CarAppController.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var app = angular.module('CarApp');
+    app.controller('CarController', function () {
+        this.saveCar = function () {
+            console.dir(this.car);
+        };
+    });
+
+})();
